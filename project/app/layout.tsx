f4ac680b6523dd67fb00877a7cc0bba6d7718c78
Sankar K G <sankarfrompalani@gmail.com>
2025-02-27 22:22:27 +0530

    feat: integrate Sanity for content management; add author and startup schema types; update layout and components for new data structure

--- project/app/layout.tsx
+++ project/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "easymde/dist/easymde.min.css";
 
 const workSans = localFont({
   src:[
--- project/components/StartUpCard.tsx
+++ project/components/StartUpCard.tsx
@@ -1,6 +1,9 @@
 import { formateDate } from '@/lib/utils'
 import { EyeIcon } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link'
 import React from 'react'
+import { Button } from './ui/button';
 
 const StartUpCard = ({post}:{post: StartUpCardType}) => {
     const {_createdAt, _id, views, author:{_id:authorId, name}, description, image, category, title} = post;
@@ -15,8 +18,30 @@ const StartUpCard = ({post}:{post: StartUpCardType}) => {
                 <span className='text-16-mwdium'>{views}</span>
             </div>
         </div>
-        <div>
-            
+        <div className='flex-between mt-5 gap-5'>
+            <div className='flex-1'>
+                <Link href={`/user/${authorId}`}>
+                    <p className='text-16-medium line-clamp-1'>{name}</p>
+                </Link>
+                <Link href={`/startup/${_id}`}>
+                    <h3 className='text-26-semibold line-clamp-2'>{title}</h3>
+                </Link>
+            </div>
+            <Link href={`/user/${authorId}`}>
+                <Image src="https://placehold.co/48x48" alt={title} width={48} height={48} className='rounded-full'/>
+            </Link>
+        </div>
+        <Link href={`/startup/${_id}`}>
+            <p className='startup-card_desc'>{description}</p>
+            <img src={image} alt="placeholder" className='startup-card_img'/>
+        </Link>
+        <div className='flex-between gap-3 mt-5'>
+            <Link href={`/?query=${category.toLowerCase()}`}>
+                <p className='text-16-medium'>{category}</p>
+            </Link>
+            <Button className='startup-card_btn' asChild>
+                <Link href={`/startup/${_id}`}>Details</Link>
+            </Button>
         </div>
     </li>
   )
